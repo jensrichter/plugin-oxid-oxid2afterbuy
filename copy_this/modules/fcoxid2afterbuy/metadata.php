38a11d37e1bbbb625c0e0698c222bd6017254d54
FATCHIP GmbH <kontakt@fatchip.de>
2017-08-27 17:40:29 +0200

    0033648: Create metadata.php

---
+++ copy_this/modules/fcoxid2afterbuy/metadata.php
@@ -0,0 +1,20 @@
+
+<?php
+ /*
+ * @package FATCHIP Oxid2Afterbuy
+ * @copyright (C) VIA-Online GmbH
+ * @author FATCHIP GmbH
+ * 
+ * This Software is the property of VIA-Online GmbH
+ * and is protected by copyright law - it is NOT Freeware.
+ *
+ * Any unauthorized use of this software without a valid license
+ * is a violation of the license agreement and will be
+ * prosecuted by civil and criminal law.
+ */
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '1.0';
+$oViewConf = oxNew("oxViewConfig");
+$sImgUrl = $oViewConf->getModuleUrl('fcoxid2afterbuy', 'fatchip.png');
